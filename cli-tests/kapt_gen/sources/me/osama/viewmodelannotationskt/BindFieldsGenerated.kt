8e4d1f64bf34d42710c3fc342c584614037e6c96
Udi Cohen <udinic@gmail.com>
2021-04-05 15:51:13 -0400

    minor

--- cli-tests/kapt_gen/sources/me/osama/viewmodelannotationskt/BindFieldsGenerated.kt
+++ cli-tests/kapt_gen/sources/me/osama/viewmodelannotationskt/BindFieldsGenerated.kt
@@ -2,8 +2,9 @@ package me.osama.viewmodelannotationskt
 
 import android.view.View
 import android.widget.TextView
+import kotlin.Unit
 
-fun bindFields(item: Bill, view: View) {
-    view.findViewById<TextView>(R.id.name).text = item.name
-    view.findViewById<TextView>(R.id.amount).text = item.amount
+public fun bindFields(item: Bill, view: View): Unit {
+  view.findViewById<TextView>(R.id.name).text = item.name
+  view.findViewById<TextView>(R.id.amount).text = item.amount
 }
