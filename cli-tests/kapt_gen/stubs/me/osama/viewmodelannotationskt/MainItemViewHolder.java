d27c5946332e7dce32708afb15fa071b7775b8c9
Udi Cohen <udinic@gmail.com>
2021-04-06 12:52:27 -0400

    updates

--- cli-tests/kapt_gen/stubs/me/osama/viewmodelannotationskt/MainItemViewHolder.java
+++ cli-tests/kapt_gen/stubs/me/osama/viewmodelannotationskt/MainItemViewHolder.java
@@ -2,7 +2,7 @@ package me.osama.viewmodelannotationskt;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001c\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\b"}, d2 = {"Lme/osama/viewmodelannotationskt/MainItemViewHolder;", "Landroid/support/v7/widget/RecyclerView$ViewHolder;", "view", "Landroid/view/View;", "(Landroid/view/View;)V", "bind", "", "item", "Lme/osama/viewmodelannotationskt/Bill;"})
+@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001c\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\b"}, d2 = {"Lme/osama/viewmodelannotationskt/MainItemViewHolder;", "Landroid/support/v7/widget/RecyclerView$ViewHolder;", "view", "Landroid/view/View;", "(Landroid/view/View;)V", "bind", "", "item", "Lme/osama/viewmodelannotationskt/Bill;"})
 public final class MainItemViewHolder extends android.support.v7.widget.RecyclerView.ViewHolder {
     
     @me.osama.vmannotation.BindField(viewIds = {"name", "amount"}, viewName = "view")
--- processor/src/main/java/me/osama/vmannotationprocessor/BindFieldsProcessor.kt
+++ processor/src/main/java/me/osama/vmannotationprocessor/BindFieldsProcessor.kt
@@ -37,8 +37,8 @@ class BindFieldsProcessor : AbstractProcessor() {
     }
 
     private fun generateNewMethod(method: ExecutableElement, variable: VariableElement, packageOfMethod: String) {
-        val generatedSourcesRoot: String = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME].orEmpty()
-//        val generatedSourcesRoot = "/Users/udinic/projects/ViewModelAnnotationsKt/cli-tests/kapt_gen/sources"
+//        val generatedSourcesRoot: String = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME].orEmpty()
+        val generatedSourcesRoot = "/Users/udinic/projects/ViewModelAnnotationsKt/cli-tests/kapt_gen/sources"
         if(generatedSourcesRoot.isEmpty()) {
             processingEnv.messager.errormessage { "UDINIC Can't find the target directory for generated Kotlin files." }
             return
